d2e3d6383a50d2e9d70a39b3bb03fec4d98bcba2
Brett Cassedy <brett.cassedy@gmail.com>
2014-05-21 15:03:54 -0700

    Horizontal profile form

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ STARKupid::Application.routes.draw do
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
   resources :profiles, only: [:new, :create]
-  get 'profiles/:username', to: 'profiles#show', as: 'profile_show'
+  get 'profiles/:username', to: 'profiles#show', as: 'profile'
 end
