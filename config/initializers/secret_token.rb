9cc94ce0a81e6036db0cdb5af6bea3138a421ff5
Brett Cassedy <brett.cassedy@gmail.com>
2014-05-21 10:23:48 -0700

    Start project!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+STARKupid::Application.config.secret_key_base = 'e6822a527103f2e3bd2c27daa37b7ab1335269fa4a356e5f4a5c5f4880a57373d5064fce47cdecb3b06b288c5182185238da83efeade0af0b85e5dcb0c8f67b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+STARKupid::Application.config.session_store :cookie_store, key: '_STARKupid_session'
