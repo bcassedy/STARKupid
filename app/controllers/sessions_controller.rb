7356491b61775bb35b5d9191cc74547fd78c5673
Brett Cassedy <brett.cassedy@gmail.com>
2014-05-22 15:52:28 -0700

    Remove deleted files from version control /

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
       redirect_to root_url
     else
       flash.now[:errors] = ['Invalid username/password']
+      @user = User.new
       render :new
     end
   end
