48a7c9b85f62d3d89b2900a86163ec3e15f0521a
Brett Cassedy <brett.cassedy@gmail.com>
2014-05-23 18:05:41 -0700

    Layout tweaks

--- app/assets/javascripts/views/message_show.js
+++ app/assets/javascripts/views/message_show.js
@@ -1,6 +1,10 @@
 STARKupid.Views.MessageShow = Backbone.View.extend({
   template: JST['messages/show'],
 
+  events: {
+    'submit #msg-form': 'submitMessage'
+  },
+
   render: function () {
     var renderedContent = this.template({
       message: this.model
@@ -8,5 +12,19 @@ STARKupid.Views.MessageShow = Backbone.View.extend({
     this.$el.html(renderedContent);
 
     return this;
+  },
+
+  submitMessage: function (event) {
+    event.preventDefault();
+    var messageData = $(event.target).serializeJSON();
+    var url = 'api/profiles/' + this.model.get('sender') + '/messages'
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: messageData,
+      success: function () {
+        $('form').prepend('<div class="alert alert-success">Message Sent</div>');
+      }
+    });
   }
 });
