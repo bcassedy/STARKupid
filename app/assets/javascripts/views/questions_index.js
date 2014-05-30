07eef5ee2c5a84525a24a8166c8ef2b393fc9980
Brett Cassedy <brett.cassedy@gmail.com>
2014-05-30 09:42:07 -0700

    Add feature to cycle through questions

--- app/assets/javascripts/views/questions_index.js
+++ app/assets/javascripts/views/questions_index.js
@@ -3,14 +3,41 @@ STARKupid.Views.QuestionsIndex = Backbone.View.extend({
     this.listenTo(this.collection, 'sync', this.render);
   },
 
-  template: JST['questions/index'],
+  events: {
+    'submit .question-form': 'submitQuestionForm'
+  },
 
   render: function () {
+    if (this.collection.length === 0) {
+      this.template = JST['questions/index'];
+    } else {
+      this.template = JST['questions/show'];
+    }
     var renderedContent = this.template({
-      questions: this.collection
+      question: this.collection.first()
     });
     this.$el.html(renderedContent);
 
     return this;
+  },
+
+  submitQuestionForm: function (event) {
+    var that = this;
+    event.preventDefault();
+    var answerData = $(event.currentTarget).serializeJSON();
+    var url = 'api/questions/' + this.collection.first().id +
+      '/answered_questions';
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: answerData,
+      success: function () {
+        that.collection.fetch({
+          success: function () {
+            Backbone.history.navigate('#/questions', { trigger: true });
+          }
+        });
+      }
+    });
   }
-})
+});
