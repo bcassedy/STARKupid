20d6a0b327ea83ffaa8abbb95192529d581aad12
Brett Cassedy <brett.cassedy@gmail.com>
2014-06-04 17:58:55 -0700

    Fix matching

--- app/controllers/api/profiles_controller.rb
+++ app/controllers/api/profiles_controller.rb
@@ -5,7 +5,7 @@ module Api
     def index
       @profiles = Profile.includes(:photos).page(params[:page])
       if current_user.profile
-        assign_match_percent(@profiles)
+        assign_match_percent
         @profiles << Profile.find_by_username(current_user.username)
       end
       render :index
@@ -13,7 +13,8 @@ module Api
 
     def show
       @profile = Profile.find_by_username(params[:id])
-      assign_match_percent([@profile])
+      @profiles = [@profile]
+      assign_match_percent
       Visit.create_or_update(current_user.profile.id, @profile.id)
       render partial: 'api/profiles/profile', locals: { profile: @profile }
     end
@@ -47,7 +48,7 @@ module Api
 
     def search
       @profiles = Profile.search_by_username(params[:query])
-      assign_match_percent(@profiles)
+      assign_match_percent
       render :search_results
     end
 
@@ -67,8 +68,8 @@ module Api
       )
     end
 
-    def assign_match_percent(profiles)
-      profiles.each do |profile|
+    def assign_match_percent
+      @profiles.each do |profile|
         break unless current_user.profile
         profile.match_percentage = current_user.profile.match(profile)
       end
